28665571b9f6646c1a1873100548fdce11fb0920
root <root@localhost.(none)>
2016-10-19 22:34:27 -0500

    Descrpcion

--- services_parcial/files.py
+++ services_parcial/files.py
@@ -1,7 +1,7 @@
 from flask import Flask, abort, request
 import json
 
-from files_commands import get_all_files, add_file, remove_file
+from files_commands import get_all_files, add_file, remove_file, get_recently_files
 
 app = Flask(__name__)
 api_url = '/v1.0'
@@ -9,14 +9,12 @@ api_url = '/v1.0'
 @app.route(api_url+'/files',methods=['POST'])
 def create_file():
   contentJson = request.get_json(silent=True)
-  filename = content['filename']
-  content  = content['content']
+  filename = contentJson['filename']
+  content  = contentJson['content']
   if not filename or not content:
     return "empty username or content", 400
-  if filename in get_all_files():
-    return "The file already exist", 400
   if add_file(filename,content):
-    return "user created", 201
+    return "HTTP 201 CREATE", 201
   else:
     return "error while creating file", 400
 
@@ -28,19 +26,36 @@ def read_file():
 
 @app.route(api_url+'/files',methods=['PUT'])
 def update_file():
-  return "not found", 404
+  return "HTTP 404 NOT FOUND", 404
 
 @app.route(api_url+'/files',methods=['DELETE'])
 def delete_file():
   error = False
-  for filename in get_all_files():
-    if not remove_file(filename):
-        error = True
-
+  error = remove_file()
   if error:
-    return 'some files were not deleted', 400
+    return 'HTTP 200 OK', 200
   else:
-    return 'all files were deleted', 200
+    return 'HTTP 400 error', 400
+
+@app.route(api_url+'/files/recently',methods=['GET'])
+def read_recent_file():
+  list = {}
+  list["files"] = get_recently_files()
+  return json.dumps(list), 200
+
+@app.route(api_url+'/files/recently',methods=['POST'])
+def create_recent_file():
+  return "HTTP 404 NOT FOUND", 404
+
+@app.route(api_url+'/files/recently',methods=['PUT'])
+def update_recent_file():
+  return "HTTP 404 NOT FOUND", 404
+
+@app.route(api_url+'/files/recently',methods=['DELETE'])
+def delete_recent_files():
+  return "HTTP 404 NOT FOUND", 404
+
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0',port=8088,debug='True')
+
--- services_parcial/files_commands.py
+++ services_parcial/files_commands.py
@@ -1,23 +1,25 @@
 from subprocess import Popen, PIPE
 
 def get_all_files():
-  grep_process = Popen(["ls","/home/filesystem_user"], stdout=PIPE, stderr=PIPE)
-  file_list =  [path.rstrip('\n') for path in p.stdout.readlines()]
+  process = Popen(["ls","/home/filesystem_user"], stdout=PIPE, stderr=PIPE)
+  file_list =  [path.rstrip('\n') for path in process.stdout.readlines()]
   return filter(None, file_list)
 
 def add_file(filename,content):
-  finalContent = '+ content +'
-  add_process = Popen(["echo",finalContent,">", filename], stdout=PIPE, stderr=PIPE)
-  add_process.wait()
+  process = Popen(["touch", "/home/filesystem_user/" + filename], stdin=PIPE, stdout=PIPE, stderr=PIPE)
+  log = open("/home/filesystem_user/" + filename, 'w')
+  log.write(content)
+  log.flush()
   return True if filename in get_all_files() else False
 
-def remove_file(filename):
-    remove_process = Popen(["sudo","rm","-r",filename], stdout=PIPE, stderr=PIPE)
-    remove_process.wait()
-    return False if username in get_all_files() else True
+
+def remove_file():
+  deleteCommand = 'find /home/filesystem_user -type f -maxdepth 1 -not -name ".*" -exec rm {} \;'  	
+  process = Popen(deleteCommand, universal_newlines=True, shell=True, stdout=PIPE, stderr=PIPE).communicate()
+  return True
 
 def get_recently_files():
-  grep_process = Popen(["ls","/home/filesystem_user"], stdout=PIPE, stderr=PIPE)
-  file_list =  [path.rstrip('\n') for path in p.stdout.readlines()]
-  file_list = file_list[:10]
+  process = Popen(["ls","/home/filesystem_user","-1t"], stdout=PIPE, stderr=PIPE)
+  file_list =  [path.rstrip('\n') for path in process.stdout.readlines()]
+  file_list = file_list[:3]
   return filter(None, file_list)
